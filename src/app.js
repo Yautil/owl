ac91cb2d2834a8ae7c4cc79075429054690b55f8
Miguel Oppermann <miguel.o@gmx.net>
2018-01-22 15:06:38 +0100

    Master init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import "./stylesheets/main.css";
+
+// Small helpers you might want to keep
+import "./helpers/context_menu";
+import "./helpers/external_links";
+
+// ----------------------------------------------------------------------------
+// Everything below is just to show you how it works. You can delete all of it.
+// ----------------------------------------------------------------------------
