46de1bfee05d8d9b2bc6c54d5896ee0bf13eb2dc
Miguel Oppermann <miguel.o@gmx.net>
2018-01-22 15:42:01 +0100

    better boilerplate

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import "./stylesheets/main.css";
-
-// Small helpers you might want to keep
-import "./helpers/context_menu";
-import "./helpers/external_links";
-
-// ----------------------------------------------------------------------------
-// Everything below is just to show you how it works. You can delete all of it.
-// ----------------------------------------------------------------------------
